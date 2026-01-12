2d0d1548dd769151bf924c9e00f7423442aa3929
TMCSEHM <92201012+TMCSEHM@users.noreply.github.com>
2026-01-12 01:08:27 -0800

    Set game to draw max of 60fps, returns correct mili-s per frame

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ from logger import log_state
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fps_clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         pygame.Surface.fill(screen, "black") # fill screen black
         pygame.display.flip() # display update
+
+        dt = fps_clock.tick(60) / 1000
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
