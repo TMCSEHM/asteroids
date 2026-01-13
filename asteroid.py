1255416df8953bfeaae8fe962930eb3306759304
TMCSEHM <92201012+TMCSEHM@users.noreply.github.com>
2026-01-13 00:35:16 -0800

    Update asteroids to split smaller in random directions after getting shot

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,10 @@
 import pygame
+import random
+from logger import log_event
 from circleshape import CircleShape
 from constants import (
         LINE_WIDTH,
+        ASTEROID_MIN_RADIUS,
 )
 
 class Asteroid(CircleShape):
@@ -13,3 +16,18 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        angle = random.uniform(20, 50)
+        first_split_vector = self.velocity.rotate(angle)
+        second_split_vector = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        one = Asteroid(self.position.x, self.position.y, new_radius)
+        two = Asteroid(self.position.x, self.position.y, new_radius)
+        one.velocity = first_split_vector * 1.2
+        two.velocity = second_split_vector * 1.2
+        
